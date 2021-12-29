2461559fbec05b9f6fa1a52ed5b045e79fd35984
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-28 19:32:57 -0500

    Revert "Revert "Remove uneeded assets; get manifest in console""
    This reverts commit 4633f8fd659ae3ef5718a52a7d0474a6e0cd579f.

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { Router } from "@roxi/routify";
 	import { routes } from "../.routify/routes";
+	import ReloadComponent from '@components/ReloadComponent.svelte'
 </script>
 
 <Router {routes} />
+<ReloadComponent />
+
 
