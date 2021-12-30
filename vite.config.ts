d9d7dfb1e23ff157b22cf3d15e4c810dd433f7aa
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-30 00:58:39 -0500

    Revert "try to solve 404 error"
    This reverts commit a3f19ea5b5fac52f3632f5ebe6509df36365134a.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const preprocess = require('svelte-preprocess')
 export default defineConfig({
   plugins: [svelte({ preprocess: preprocess() })],
   publicDir: './assets/',
-  base: '/dashbox/',
+  // base: '/dashbox/',
   build: {
     outDir: './public/'
   },
