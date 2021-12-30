755e47cc2a23683de4fdda79bf512513f3b99459
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-29 23:30:43 -0500

    try with new base

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const preprocess = require('svelte-preprocess')
 export default defineConfig({
   plugins: [svelte({ preprocess: preprocess() })],
   publicDir: './assets/',
-  base: '/dashbox/',
+  base: 'https://pfesenmeier.github.io/dashbox/',
   build: {
     outDir: './public/'
   },
